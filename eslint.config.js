922c14cff6e0d0edc8e14dd564bd3d35eabae625
riotDev17 <tomasalberto527@gmail.com>
2024-02-05 09:42:58 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    rules: {
+      semi: 'error',
+      'no-unused-vars': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'require-await': 'error',
+    },
+    files: ['src/**/*.js'],
+  },
+];
