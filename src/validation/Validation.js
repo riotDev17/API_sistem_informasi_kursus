a130f28d9390855361e81524f3636fdd72b54f92
riotDev17 <tomasalberto527@gmail.com>
2024-02-05 09:59:31 +0700

    feat: membuat validation untuk menangani error yang terjadi saat penginputan

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,4 +1,4 @@
-import { ResponseError } from '../error/responseError.js';
+import { ResponseError } from '../error/ResponseError.js';
 
 const Validation = (schema, request) => {
   const result = schema.validate(request, {
