a47395652d14f676d0b8f3d6b901f58f54d7ab08
riotDev17 <tomasalberto527@gmail.com>
2024-02-16 21:33:46 +0700

    chore: menghapus Provinsi , Kecamatan , Kabupaten , Kelurahan

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -4,11 +4,7 @@ import AgamaController from '../controller/AgamaController.js';
 import KursusController from '../controller/KursusController.js';
 import SekolahController from '../controller/SekolahController.js';
 import PengajarController from '../controller/PengajarController.js';
-import ProvinsiController from '../controller/ProvinsiController.js';
-import KabupatenController from '../controller/KabupatenController.js';
 import PekerjaanController from '../controller/PekerjaanController.js';
-import KecamatanController from '../controller/KecamatanController.js';
-import KelurahanController from '../controller/KelurahanController.js';
 import PengumumanController from '../controller/PengumumanController.js';
 import PendidikanController from '../controller/PendidikanController.js';
 import PembayaranController from '../controller/PembayaranController.js';
@@ -80,70 +76,6 @@ Router.put(
 Router.delete(
   '/api/penghasilan/:penghasilanId',
   PenghasilanController.DeletePenghasilanController,
-);
-
-// PROVINSI
-Router.get('/api/provinsi', ProvinsiController.GetProvinsiController);
-Router.post('/api/provinsi', ProvinsiController.CreateProvinsiController);
-Router.get(
-  '/api/provinsi/:provinsiId',
-  ProvinsiController.GetProvinsiByIdController,
-);
-Router.put(
-  '/api/provinsi/:provinsiId',
-  ProvinsiController.UpdateProvinsiController,
-);
-Router.delete(
-  '/api/provinsi/:provinsiId',
-  ProvinsiController.DeleteProvinsiController,
-);
-
-// KABUPATEN
-Router.get('/api/kabupaten', KabupatenController.GetKabupatenController);
-Router.post('/api/kabupaten', KabupatenController.CreateKabupatenController);
-Router.get(
-  '/api/kabupaten/:kabupatenId',
-  KabupatenController.GetKabupatenByIdController,
-);
-Router.put(
-  '/api/kabupaten/:kabupatenId',
-  KabupatenController.UpdateKabupatenController,
-);
-Router.delete(
-  '/api/kabupaten/:kabupatenId',
-  KabupatenController.DeleteKabupatenController,
-);
-
-// KECAMATAN
-Router.get('/api/kecamatan', KecamatanController.GetKecamatanController);
-Router.post('/api/kecamatan', KecamatanController.CreateKecamatanController);
-Router.get(
-  '/api/kecamatan/:kecamatanId',
-  KecamatanController.GetKecamatanByIdController,
-);
-Router.put(
-  '/api/kecamatan/:kecamatanId',
-  KecamatanController.UpdateKecamatanController,
-);
-Router.delete(
-  '/api/kecamatan/:kecamatanId',
-  KecamatanController.DeleteKecamatanController,
-);
-
-// KELURAHAN
-Router.get('/api/kelurahan', KelurahanController.GetKelurahanController);
-Router.post('/api/kelurahan', KelurahanController.CreateKelurahanController);
-Router.get(
-  '/api/kelurahan/:kelurahanId',
-  KelurahanController.GetKelurahanByIdController,
-);
-Router.put(
-  '/api/kelurahan/:kelurahanId',
-  KelurahanController.UpdateKelurahanController,
-);
-Router.delete(
-  '/api/kelurahan/:kelurahanId',
-  KelurahanController.DeleteKelurahanController,
 );
 
 // SEKOLAH
