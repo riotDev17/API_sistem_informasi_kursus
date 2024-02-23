95b34cbe8b4f5c1232c33ba94a9a01f1839d2991
riotDev17 <tomasalberto527@gmail.com>
2024-02-23 21:05:10 +0700

    users validation

--- src/validation/UsersValidation.js
+++ src/validation/UsersValidation.js
@@ -24,10 +24,10 @@ const UpdateUserValidation = Joi.object({
   tempat_lahir: Joi.string().required(),
   tanggal_lahir: Joi.string().max(50).required(),
   jenis_kelamin: Joi.string().max(50).required(),
-  no_telepon: Joi.string().max(25).required(),
+  no_telepon: Joi.string().max(25).optional(),
   alamat: Joi.string().required(),
-  instagram: Joi.string().required(),
-  whatsapp: Joi.string().max(25).required(),
+  instagram: Joi.string().optional(),
+  whatsapp: Joi.string().max(25).optional(),
 });
 
 const GetUsersValidation = Joi.string().max(100);
